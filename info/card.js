c2869b29771c9cf8833d6f75ffe54ab5cc83620c
Furlin <vitor@furlin.org>
2014-04-04 18:00:53 -0300

    trustAsResourceUrl

--- info/card.js
+++ info/card.js
@@ -11,6 +11,8 @@ angular.module('card', ['ngTouch'])
     .success(function (data, status, headers, config) {
         $scope.card = data;
         $scope.lang = 'none';
+        $scope.img_us = $sce.trustAsResourceUrl(data[0].img_us);
+        $scope.img_br = $sce.trustAsResourceUrl(data[0].img_br);
         $scope.text_us = $sce.trustAsHtml(data[0].text_us);
         $scope.text_br = $sce.trustAsHtml(data[0].text_br);
         if($scope.text_br != null) {
