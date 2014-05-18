223f247b0b54b47d0247aecf516635fa81c3d538
Furlin <vitor@furlin.org>
2014-05-18 13:43:49 -0300

    def font

--- info/collection.js
+++ info/collection.js
@@ -9,15 +9,25 @@ angular.module('card', ['ngTouch'])
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }])
 .controller('CollectionCtrl', ['$scope', '$http', '$sce', '$window', function($scope, $http, $sce, $window) {
+    if($window.localStorage.getItem('user') == null)
+        $window.location.href = 'login.html';
+    else
+        $scope.user = $window.localStorage.getItem('user');
+    
+    var urlText = $window.location.search;
+    var startPos = urlText.indexOf('?');
+    if(startPos == -1) var sign = '?';
+    else var sign = '&';
+    
     $scope.deck = {'deck':'Carregando informações...'};
     $scope.deck.show = false;
     $scope.refr = function () {
         $window.location.href = 'collection.html'+$window.location.search;
     }
-    $http.get('http://furlin.org/hobby/collection.php'+$window.location.search)
+    $http.get('http://furlin.org/hobby/collection.php'+$window.location.search+sign+'user='+$window.localStorage.getItem('user'))
     .success(function (data, status, headers, config) {
-        $scope.deck.show = true;
         $scope.deck = data;
+        $scope.deck.show = true;
         console.log(data);
         $scope.goTo = function (link) {
             $window.location.href = link;  
